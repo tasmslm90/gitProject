5174bb803ee8eb7256b4b6734ad4fa371619e3a9
tasmuslum90 <tasmslm1990@gmail.com>
2023-05-31 15:57:12 +0200

    changenew

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         int i = 5;
-            System.out.println("i = " + i + i);
+            System.out.println("i = " + i + i +1) ;
 
     }
 }
