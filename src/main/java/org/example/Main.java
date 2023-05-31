a5ef8363bb5ece70d8524eafe5324efaf96a359e
tasmuslum90 <tasmslm1990@gmail.com>
2023-05-31 15:39:58 +0200

    change

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        int i = 1;
+        int i = 5;
             System.out.println("i = " + i);
 
     }
