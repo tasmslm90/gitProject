788e970cdb79a518f8b20e83f6a42844e6c583b8
tasmuslum90 <tasmslm1990@gmail.com>
2023-06-01 15:39:00 +0200

    addtest1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
         int i = 5;
             System.out.println("i = " + i + i +1) ;
-            System.out.println("gittest");
+            System.out.println("gittest1");
 
     }
 }
